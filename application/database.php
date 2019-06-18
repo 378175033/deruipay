f7d4bb445e158450e813a3f9d4dc0a5b41d39031
ZhouJing <614434346@qq.com>
2019-06-18 11:36:50 +0800

    database

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'www_f4_com',
+    'database'        => 'demo',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '6666 ',
+    'password'        => '6666',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
