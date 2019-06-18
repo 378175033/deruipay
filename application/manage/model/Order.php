16a0df02ebd817106cd6db379f7a9b42190e456a
ZhouJing <614434346@qq.com>
2019-06-18 17:55:57 +0800

    database

---
+++ application/manage/model/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/14 0014
+ * Time: 13:41
+ */
+
+namespace app\manage\model;
+use think\Model;
+
+
+class Order extends Model
+{
+    protected $autoWriteTimestamp = true;
+}
