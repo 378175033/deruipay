e1280a9703dd1506ffea923ac61fcb2cdfae8d5e
ZhouJing <614434346@qq.com>
2019-07-02 16:30:00 +0800

    dspay

--- application/manage/controller/Daxiangpay.php
+++ application/manage/controller/Daxiangpay.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace app\manage\controller;
+
 use think\Controller;
 use daxiangpay\daxiangpay as api;
+use think\Request;
 
 class Daxiangpay extends controller
 {
@@ -10,15 +13,36 @@ class Daxiangpay extends controller
         $data = ['amount' => '0.01', 'bankname' => '建设银行', 'bankcardid' => '6217003810031856423', 'bankfullname' => '胡译锋', 'bankidc' => '510902199301219536', 'bankmobile' => '15708432599', 'screen' => 1];
         $this->pay($data);
     }
-    
+
     public function pay($data)
     {
         $api = new api();
         $api->pay($data);
     }
 
-    public function notify()
+    public function notify(Request $request)
     {
-        
+        $api = new api();
+        dump($request->param());
+        $paidInfo = $request->post();
+        $attach = $paidInfo['attach'];
+        if (strlen($attach) > 0) {
+            $attach = base64_decode($attach);
+        }
+        unset($paidInfo['attach']);
+        $paidInfo = $api->signChk($paidInfo);
+        if ($paidInfo === false) {
+            echo '验签失败';
+        } elseif ($paidInfo['paystate'] == 'success') {//支付成功
+            echo '_CC_PAY_STATE_SUCCESS_';
+
+            $orderId = $paidInfo['orderid'];    //发起支付时发送过去的 订单号
+            $amount = floatval($paidInfo['amount']);    //支付金额
+            $tradeId = $paidInfo['tradeid'];    //支付平台的流水号
+
+        } else {
+            echo $paidInfo['paystate'];
+            echo '非支付成功状态';
+        }
     }
 }
