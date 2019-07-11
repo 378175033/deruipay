7b29831584a7bf2b0727ceca4593e3062751c4e4
zhl <123456>
2019-07-11 17:32:39 +0800

    cs

--- application/manage/validate/User.php
+++ application/manage/validate/User.php
@@ -12,6 +12,7 @@ use think\Validate;
 class User extends Validate
 {
     protected $rule = [
-        ['username','require|unique:user',"用户名/登录名不能为空|用户名已存在"],
+//        ['username','require|unique:user',"用户名/登录名不能为空|用户名已存在"],
+        ['mobile','require|unique:user',"手机号不能为空|手机号已存在"],
     ];
 }
