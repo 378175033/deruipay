b1fa7a99edca0a9d3c3e22c1bb4e53e3c2bf92fd
zhl <123456>
2019-07-15 16:35:10 +0800

    cs

--- application/business/controller/User.php
+++ application/business/controller/User.php
@@ -212,6 +212,12 @@ class User extends Business
      */
     public function withdraw()
     {
+        //判断是否开启提现
+        $open_withdraw = deploy("open_withdraw");
+        if( empty($open_withdraw) || empty( $this->user['withdraw'] )){
+            $this->error( "未开启提现通道，请联系客服开启！", url("Index/welcome"));
+        }
+
         $id = $this->user['id'];
         //校验安全密码
         if( empty( $this->user['safe_pass'] ) ){
@@ -219,7 +225,7 @@ class User extends Business
         }
         if( $this->request->isAjax() && $this->request->isPost() )
         {
-            $param = $this->request->param();
+            $param = $this->request->post();
             $param['bus_id'] = $id;
             //判断安全密码是否正确
             if( request()->param('safe_pass', "") != $this->user['safe_pass'] ){
--- application/business/model/AccountLog.php
+++ application/business/model/AccountLog.php
@@ -15,7 +15,7 @@ class AccountLog extends Model
 {
     public function getInfoAttr($value)
     {
-        $status = [0=>'订单录入',1=>'平台设置余额',2=>'平台设置冻结金额',3=>'申请提现',4=>'申请提现返还'];
+        $status = [0=>'订单录入',1=>'平台设置余额',2=>'平台设置冻结金额',3=>'申请提现',4=>'申请提现返还',5=>"提现手续费",6=>"提现手续费返回"];
         return $status[$value];
     }
 }
